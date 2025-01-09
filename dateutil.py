a943aa3cb622338e530e9ad4e3952c76f1303ee9
rosalin140219 <161487245+rosalin140219@users.noreply.github.com>
2025-01-09 10:38:26 +0800

    fix 时间戳

--- dateutil.py
+++ dateutil.py
@@ -2,14 +2,23 @@ from datetime import datetime
 
 
 def format_date_with_microseconds(time_string):
-    time_format = '%Y-%m-%d %H:%M:%S'
-    # 解析时间字符串为 datetime 对象
-    parsed_time = datetime.strptime(time_string, time_format)
-
-    # 添加微秒部分 (例如，添加 0 微秒)
-    time_with_microseconds = parsed_time.replace(microsecond=0)
+    # 尝试解析带微秒的时间字符串
+    try:
+        time_format_with_microseconds = '%Y-%m-%d %H:%M:%S.%f'
+        parsed_time = datetime.strptime(time_string, time_format_with_microseconds)
+    except ValueError:
+        # 如果失败，尝试解析不带微秒的时间字符串
+        time_format_without_microseconds = '%Y-%m-%d %H:%M:%S'
+        parsed_time = datetime.strptime(time_string, time_format_without_microseconds)
+        # 添加微秒部分 (例如，添加 0 微秒)
+        parsed_time = parsed_time.replace(microsecond=0)
 
     # 将 datetime 对象转换回带微秒的字符串格式
-    time_string_with_microseconds = time_with_microseconds.strftime('%Y-%m-%d %H:%M:%S.%f')
+    time_string_with_microseconds = parsed_time.strftime('%Y-%m-%d %H:%M:%S.%f')
 
     return time_string_with_microseconds
+
+
+if __name__ == '__main__':
+    time_string = '2025-01-09 02:31:03'
+    print(format_date_with_microseconds(time_string))
